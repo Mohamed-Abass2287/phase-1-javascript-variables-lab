56faabce5da600890306271c8eb977fd0692593a
mohamed abass tawana <aminabass987@gmail.com>
2025-03-05 14:08:49 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// Define companyName as a const and set it to 'Scuber'
+const companyName = 'Scuber';
+
+// Define mostProfitableNeighborhood using let and set it to 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define companyCeo using let and set it to 'Susan Smith'
+let companyCeo = 'Susan Smith';
